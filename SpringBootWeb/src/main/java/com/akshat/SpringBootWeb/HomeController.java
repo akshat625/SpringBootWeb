a09ec246acfae97c8a2a16cd10f27b5e64a1be58
Akshat Srivastava <akshatsrivastava625@gmail.com>
2024-07-25 15:34:15 +0530

    Prefix/Suffix

--- SpringBootWeb/src/main/java/com/akshat/SpringBootWeb/HomeController.java
+++ SpringBootWeb/src/main/java/com/akshat/SpringBootWeb/HomeController.java
@@ -1,8 +1,10 @@
 package com.akshat.SpringBootWeb;
 
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +14,7 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         System.out.println("Home Page Loaded");
-        return "index.jsp";
+        return "index";
     }
 
 
@@ -28,14 +30,29 @@ public class HomeController {
 //        return "result.jsp";
 //    }
 
-    //Spring boot way
-    @RequestMapping("add")
-    //RequestParam is optional, used when we want to use different name for variable
-    public String add(@RequestParam("num1") int num, int num2, HttpSession session){
 
+
+    //Spring boot way
+//    @RequestMapping("add")
+    //RequestParam is optional, used when we want to use different name for variable
+//    public String add(@RequestParam("num1") int num, int num2, HttpSession session){
+//
+//        int result = num + num2;
+//
+//        session.setAttribute("result", result);
+//        return "result.jsp";
+//    }
+
+
+
+
+    //Model Object
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int num, int num2, Model model){
         int result = num + num2;
 
-        session.setAttribute("result", result);
-        return "result.jsp";
+//        session.setAttribute("result", result);
+        model.addAttribute("result",result);
+        return "result";
     }
 }
